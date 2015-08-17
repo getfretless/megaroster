fb37badeb133d234db7ef249b49fb2c43eb121e1
Dave Strus <dave@davestrus.com>
2015-08-17 16:34:04 -0400

    Add students name to an array and an ordered list.

--- js/megaroster.js
+++ js/megaroster.js
@@ -1,7 +1,27 @@
 var Megaroster = function() {
+  this.veggies = ['cabbage', 'lettuce'];
+
+
   this.init = function() {
-    alert('Whaaaaat?');
-  }
+    var self = this;
+    this.students = [];
+
+
+    $('#new_student_form').on('submit', function (ev) {
+      ev.preventDefault();
+      var student_name = $(this.student_name).val();
+
+      // Push the student name onto the students array
+      self.students.push(student_name);
+
+      // Use console.log to prove that the array has a new student
+      console.log(self.students.length);
+
+      // Add the student to a new list item in the <ol>
+      $('#students').append('<li class="list-group-item">' + student_name + '</li>');
+    });
+  };
+
 };
 
 var roster = new Megaroster();
