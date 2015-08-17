28c932b00d57fae15de95aac34030692d8473866
Dave Strus <dave@davestrus.com>
2015-08-17 17:45:40 -0400

    Save to and load from localStorage.

--- js/megaroster.js
+++ js/megaroster.js
@@ -1,18 +1,46 @@
 var Megaroster = function() {
-  this.veggies = ['cabbage', 'lettuce'];
+  var self = this;
 
+  this.save = function() {
+    try {
+      return (localStorage.students = JSON.stringify(self.students));
+    }
+    catch(err) {
+      return false;
+    }
+  };
+
+  this.load = function() {
+    try {
+      self.students = JSON.parse(localStorage.students);
+      $.each(self.students, function(index, student_name) {
+        self.appendToList(student_name);
+      });
+    }
+    catch(err) {
+      return false;
+    }
+  };
+
+  this.appendToList = function(student_name) {
+    $('#students').append('<li class="list-group-item">' + student_name + '</li>');
+  };
+
+  this.addStudent = function(student_name) {
+    self.students.push(student_name);
+    self.appendToList(student_name);
+  };
 
   this.init = function() {
-    var self = this;
-    this.students = [];
-
+    self.students = [];
+    self.load();
 
     $('#new_student_form').on('submit', function (ev) {
       ev.preventDefault();
       var student_name = $(this.student_name).val();
-      self.students.push(student_name);
 
-      $('#students').append('<li class="list-group-item">' + student_name + '</li>');
+      self.addStudent(student_name);
+
       $(this.student_name)
         .val('')
         .focus();
